2be24b12cb211b00f4daf7fe3565a5f8cf7b5cb2
Gene Boggs <gene@ology.net>
2021-04-24 06:10:14 -0700

    initial commit

---
+++ lib/idi.pm
@@ -0,0 +1,33 @@
+package idi;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+idi - Module abstract placeholder text
+
+=head1 SYNOPSIS
+
+=for comment Brief examples of using the module.
+
+=head1 DESCRIPTION
+
+=for comment The module's description.
+
+=head1 AUTHOR
+
+Gene Boggs <gene@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2021 by Gene Boggs.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+

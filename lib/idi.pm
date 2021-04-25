0ea78786ec15f151841d865ac1eed0db3ca26534
Gene Boggs <gene@ology.net>
2021-04-25 00:08:34 -0700

    Add functions

--- lib/idi.pm
+++ lib/idi.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use MIDI::Simple ();
+use Music::Tempo qw(bpm_to_ms);
 
 our $VERSION = '0.001000';
 
@@ -13,13 +14,47 @@ idi - Easy Command-line MIDI
 
 =head1 SYNOPSIS
 
-  perl -Midi -E'n("ten","C4") for 1..3; n("qn","D4"); r("qn"); n("en","E4") for 1..2'
+  perl -Midi -E'n(qw(ten C4)) for 1..3; n(qw(qn D4)); r("qn"); n(qw(en E4)) for 1..2'
 
   timidity idi.mid
 
 =head1 DESCRIPTION
 
 Easy Command-line MIDI
+
+=head1 FUNCTIONS
+
+=head2 b
+
+BPM
+
+=head2 c
+
+Channel
+
+=head2 o
+
+Octave
+
+=head2 n
+
+Note
+
+=head2 p
+
+Patch
+
+=head2 r
+
+Rest
+
+=head2 t
+
+Time signature
+
+=head2 v
+
+Volume
 
 =cut
 
@@ -29,8 +64,45 @@ sub END {
   $score->write_score('idi.mid');
 }
 
+sub b {
+    my ($bpm) = @_;
+    $score->set_tempo(bpm_to_ms($bpm) * 1000);
+}
+
+sub c {
+    my ($channel) = @_;
+    $score->Channel($channel);
+}
+
+sub o {
+    my ($octave) = @_;
+    $score->Octave($octave);
+}
+
 sub n { $score->n(@_) }
+
+sub p {
+    my ($channel, $patch) = @_;
+    $score->patch_change($channel, $patch);
+}
+
 sub r { $score->r(@_) }
+
+sub t {
+    my ($signature) = @_;
+    my ($beats, $divisions) = split /\//, $signature;
+    $score->time_signature(
+        $beats,
+        ($divisions == 8 ? 3 : 2),
+        ($divisions == 8 ? 24 : 18 ),
+        8
+    );
+}
+
+sub v {
+    my ($volume) = @_;
+    $score->Volume($volume);
+}
 
 1;
 
